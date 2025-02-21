b4aa4bdc0c972c49adef64b637876fda091517a0
sajitha-developer <shajithabegam202@gmail.com>
2025-02-21 00:19:36 -0800

    update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -6,9 +6,10 @@ const Hero = () => {
     <section className='sub_header'>
     <div className='container__s1'>
         <h1>Hi...❤️, </h1>
-        <h1>I am</h1>
+        <h1>I'm</h1>
         <h1 style={{color: " #ffcc00",textAlign:'center'}}>Shajitha Begam </h1>
         <p>I'm a Front-End developer</p>
+
     <div className='container__icons'>
     <p><i className="fa-brands fa-linkedin"></i></p>
     <p><i className="fab fa-github"></i></p>
